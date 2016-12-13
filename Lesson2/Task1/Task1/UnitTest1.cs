f62ba5418bcd5879369e70b97efff32ad137850a
Karina Kudelina <cary.ua@gmail.com>
2016-12-13 17:48:36 +0200

    Open and close browser with Url

---
+++ Lesson2/Task1/Task1/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace Task1
+{
+    [TestFixture]
+    public class MyFirstTest
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            driver.Url = "http://bukovel.com/ru/";
+            wait.Until(ExpectedConditions.TitleIs("Буковель. Горнолыжный и SPA курорт в Карпатах"));
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+
+}
