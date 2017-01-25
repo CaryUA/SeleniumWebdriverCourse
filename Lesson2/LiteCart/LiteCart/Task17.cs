088c4953a280dfeec7eebb70c8f7e607d688ba5e
Karina Kudelina <cary.ua@gmail.com>
2017-01-26 00:48:49 +0200

    Tasks 17, 19 added

---
+++ Lesson2/LiteCart/LiteCart/Task17.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace LiteCart
+{
+    [TestFixture]
+    public class Task17
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void CheckBrowserLogs()
+        {
+            driver.Url = "http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1";
+            IWebElement element = wait.Until(d => d.FindElement(By.Name("username")));
+            element.SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+            element = wait.Until(d => d.FindElement(By.CssSelector("table.dataTable")));
+
+            var rows = element.FindElements(By.XPath(".//tr[.//a[contains(@href, 'product_id')]]"));
+
+            List<string> links = new List<string>();
+
+            for (int i = 0; i < rows.Count; i++)
+                links.Add(rows[i].FindElement(By.CssSelector("a[href*=product_id]")).GetAttribute("href"));
+
+            for (int i = 0; i < links.Count; i++)
+            {
+                driver.Url = links[i];
+                foreach (LogEntry l in driver.Manage().Logs.GetLog("browser"))
+                {
+                   Debug.WriteLine(l);
+                }
+            }
+        }
+
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
---
+++ Lesson2/LiteCart/LiteCart/Task19.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.Generic;
+using System.Diagnostics;
+using AutomatedTester.BrowserMob.HAR;
+using AutomatedTester.BrowserMob;
+
+namespace LiteCart
+{
+    [TestFixture]
+    public class Task19
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+        private Client client;
+        private Server server;
+
+
+        [SetUp]
+        public void start()
+        {
+            server = new Server(@"C:\Tools\browsermob-proxy-2.0-beta-6\bin\browsermob-proxy.bat");
+            server.Start();
+
+            client = server.CreateProxy();
+            client.NewHar("google");
+
+            var seleniumProxy = new Proxy { HttpProxy = client.SeleniumProxy };
+            var options = new ChromeOptions();
+            options.Proxy = seleniumProxy;
+            driver = new ChromeDriver(options);
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+
+        [Test]
+        public void ProxyTest()
+        {
+            driver.Url = "http://google.com";
+            HarResult harData = client.GetHar();
+            foreach (var en in harData.Log.Entries)
+            {
+                Debug.WriteLine(en.Response.Status.ToString() + " " + en.Response.StatusText + " " + en.Request.Url);
+            }
+        }
+
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+            client.Close();
+            server.Stop();
+        }
+    }
+}
